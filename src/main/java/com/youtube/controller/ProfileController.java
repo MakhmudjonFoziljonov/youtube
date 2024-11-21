c0988fe1c5c46f90a98cecdae224a0e422255539
zawkindev <zawkindev@gmail.com>
2024-11-21 19:28:33 +0500

    merge

--- src/main/java/com/youtube/controller/ProfileController.java
+++ src/main/java/com/youtube/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package com.youtube.controller;
 
-import com.youtube.config.AppConfig;
 import com.youtube.dto.AuthRequestDTO;
 import com.youtube.dto.AuthResponseDTO;
 import com.youtube.dto.ProfileDTO;
@@ -13,31 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
-    private final ProfileService profileService;
+    @Autowired
+    private ProfileService profileService;
 
-    public ProfileController(ProfileService profileService) {
-        this.profileService = profileService;
-    }
-    @PostMapping("")
-    public ResponseEntity<ProfileDTO> addProfile(@RequestBody @Valid ProfileCreationDTO requestDTO) {
-        return ResponseEntity.status(201).body(profileService.createProfile(requestDTO));
     @PostMapping("/registration")
     public ResponseEntity<ProfileDTO> create(@RequestBody ProfileDTO dto) {
         ProfileDTO result = profileService.registration(dto);
         return ResponseEntity.ok(result);
     }
-    @PostMapping("/change-password")
-     public ResponseEntity<String> changePassword(@RequestBody @Valid ChangePasswordDTO changePasswordDTO) {
-        boolean isChanged = profileService.changePassword(changePasswordDTO.getOldPassword(), changePasswordDTO.getNewPassword(), changePasswordDTO.getConfirmPassword());
-        if (isChanged) {
-            return ResponseEntity.ok("Password successfully updated");
-        }else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password change failed");
 
     @PostMapping("/authorization")
     public ResponseEntity<AuthResponseDTO> authorization(@RequestBody AuthRequestDTO dto,
                                                          @RequestHeader(value = "Accept-Language", defaultValue = "UZ") AppLang lang) {
-        AuthResponseDTO result = profileService.authorization(dto,lang);
+        AuthResponseDTO result = profileService.authorization(dto, lang);
         return ResponseEntity.ok(result);
     }
 }
--- src/main/java/com/youtube/service/ProfileService.java
+++ src/main/java/com/youtube/service/ProfileService.java
@@ -9,16 +9,13 @@ import com.youtube.entity.ProfileEntity;
 import com.youtube.enums.AppLang;
 import com.youtube.repository.ProfileRepository;
 import com.youtube.util.JwtUtil;
-import com.youtube.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
