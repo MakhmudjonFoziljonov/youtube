ce0945f8f413b0d4bfe49d86d3e357b0e9d7b6eb
makhmudfoziljonov <mahmudjonfoziljonov1@gmail.com>
2024-11-28 23:43:48 +0500

    comment in progress

---
+++ src/main/java/com/youtube/service/CommentService.java
@@ -0,0 +1,11 @@
+package com.youtube.service;
+
+import com.youtube.dto.CommentDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    public CommentDto create(CommentDto dto) {
+        return null;
+    }
+}
